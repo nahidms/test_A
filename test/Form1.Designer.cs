6b7ca1513e2fdf2ac46f76430092ed6e691af726
user1 <user1@LAPTOP-9NGIALNR>
2023-05-30 09:41:16 +0400

    Add project files.

---
+++ test/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboCompanyName = new System.Windows.Forms.ComboBox();
+            this.bttnShow = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choose Company:";
+            // 
+            // cboCompanyName
+            // 
+            this.cboCompanyName.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboCompanyName.FormattingEnabled = true;
+            this.cboCompanyName.Location = new System.Drawing.Point(239, 71);
+            this.cboCompanyName.Name = "cboCompanyName";
+            this.cboCompanyName.Size = new System.Drawing.Size(244, 36);
+            this.cboCompanyName.TabIndex = 1;
+            // 
+            // bttnShow
+            // 
+            this.bttnShow.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttnShow.Location = new System.Drawing.Point(512, 63);
+            this.bttnShow.Name = "bttnShow";
+            this.bttnShow.Size = new System.Drawing.Size(113, 48);
+            this.bttnShow.TabIndex = 2;
+            this.bttnShow.Text = "Show";
+            this.bttnShow.UseVisualStyleBackColor = true;
+            this.bttnShow.Click += new System.EventHandler(this.bttnShow_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 132);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 26;
+            this.dataGridView1.Size = new System.Drawing.Size(595, 237);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.Red;
+            this.lblResult.Location = new System.Drawing.Point(58, 390);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 28);
+            this.lblResult.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.bttnShow);
+            this.Controls.Add(this.cboCompanyName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboCompanyName;
+        private System.Windows.Forms.Button bttnShow;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ test/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+
+        // connection
+        string strCon = "server=.\\SQLEXPRESS;database=NahidDB;trusted_connection=true";
+
+        SqlConnection con;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DispalyCompanyID();
+        }
+
+        void DispalyCompanyID()
+        {
+
+            // check the connection state 
+            con = new SqlConnection(strCon);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            // sql query
+            string sqlQry = "select CompanyName from CompanyDetails";
+
+            // data adapter
+            SqlDataAdapter DA = new SqlDataAdapter(sqlQry, con);
+
+            // data set
+            DataSet ds = new DataSet();
+
+
+            try
+            {
+                DA.Fill(ds);
+                this.cboCompanyName.DataSource = ds.Tables[0];
+                this.cboCompanyName.DisplayMember = "companyName";
+
+            }
+
+            catch (Exception ex)
+            {
+                this.lblResult.Text = ex.Message;
+
+            }
+
+            finally
+            {
+                con.Close();
+                DA.Dispose();
+
+            }
+
+        
+
+
+    }
+
+        private void bttnShow_Click(object sender, EventArgs e)
+        {
+
+            // connection
+            con = new SqlConnection(strCon);
+
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+
+            // sql query
+            string sql = "select * from carDetails ca inner join CompanyDetails Com on ca.CompanyID = com.CompanyID where companyName='" + cboCompanyName.Text.Replace("'", "''") + "'  ";
+
+
+
+            // make adapter
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+
+
+            DataSet ds = new DataSet();
+
+
+            try
+            {
+                da.Fill(ds, "com");
+
+                this.dataGridView1.DataSource = ds;
+                this.dataGridView1.DataMember = "com";
+            }
+
+            catch (Exception ex)
+            {
+                this.lblResult.Text = ex.Message;
+            }
+
+            finally
+            {
+                con.Close();
+                da.Dispose();
+                ds.Dispose();
+            }
+
+            this.lblResult.Text = this.dataGridView1.Rows.Count - 1 + " Record(s) Found...!";
+
+
+
+        }
+    }
+    
+}
